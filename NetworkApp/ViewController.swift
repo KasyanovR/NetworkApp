893ee73b2ec82090dcf8c99c2689f4af544692a4
Kasyanov Ruslan <ri.kasyanov@gmail.com>
2020-01-10 17:05:41 +0300

    Initial Commit

---
+++ NetworkApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkApp
+//
+//  Created by User on 10/01/2020.
+//  Copyright © 2020 Ruslan Kasyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
